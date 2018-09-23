0fdf03617668830025290c809122ff2ce9840ceb
albin <albinpkr96@gmail.com>
2018-09-23 22:20:30 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+var count = 0;
+$('.contact').hide(0);
+$("*").on('wheel', function(e){
+  console.log(count);
+  count = count + (e.originalEvent.deltaY)/3;
+  $('.second').toggleClass('second first');
+  $('.hidden').removeClass('hidden');
+  if(count >= 150 ){
+  	$('.about').fadeOut(500,'linear');
+  	$('.about').hide(0);
+  	$('.contact').show();
+  }
+  else{
+  	$('.contact').fadeOut(500,'linear');
+  	$('.contact').hide(0);
+  	$('.about').show();
+  	
+  }
+  if(count > 170){
+  	count = 165;
+  }
+  if(count <= 0){
+  	count = 50;
+  }
+ 
+});
+
+// $(".test").trigger(event);
+
+$('.aboutLink').click(function(){
+	$('.contact').fadeOut(500,'linear');
+  	$('.contact').hide(0);
+  	$('.about').show();
+});
+
+$('.contactLink').click(function(){
+	$('.about').fadeOut(500,'linear');
+  	$('.about').hide(0);
+  	$('.contact').show();
+});
