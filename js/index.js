09e56af9214c895e878d9a5c883bce7a151397dc
albin <albinpkr96@gmail.com>
2018-09-23 22:55:18 +0530

    adding touch features

--- js/index.js
+++ js/index.js
@@ -38,3 +38,33 @@ $('.contactLink').click(function(){
   	$('.about').hide(0);
   	$('.contact').show();
 });
+ //touch screen
+ $("*").on('touchmove', function(e){
+  console.log(count);
+  count = count + (e.originalEvent.deltaY)/3;
+  $('.second').toggleClass('second first');
+  $('.hidden').removeClass('hidden');
+  if(count >= 150 ){
+    $('.about').fadeOut(500,'linear');
+    $('.about').hide(0);
+    $('.contact').show();
+  }
+  else{
+    $('.contact').fadeOut(500,'linear');
+    $('.contact').hide(0);
+    $('.about').show();
+    
+  }
+  if(count > 170){
+    count = 165;
+  }
+  if(count <= 0){
+    count = 50;
+  }
+ 
+});
+
+// if("ontouchstart" in window){
+//    el.addEventListener('touchmove', touchMoveHandler);
+// }
+
