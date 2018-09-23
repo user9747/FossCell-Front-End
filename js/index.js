b31a04e5506a40ba4ee371a5e95190728b6b2640
albin <albinpkr96@gmail.com>
2018-09-23 23:07:43 +0530

    adding touch features

--- js/index.js
+++ js/index.js
@@ -1,29 +1,30 @@
 var count = 0;
 $('.contact').hide(0);
-// $("*").on('wheel', function(e){
-//   console.log(count);
-//   count = count + (e.originalEvent.deltaY)/3;
-//   $('.second').toggleClass('second first');
-//   $('.hidden').removeClass('hidden');
-//   if(count >= 150 ){
-//   	$('.about').fadeOut(500,'linear');
-//   	$('.about').hide(0);
-//   	$('.contact').show();
-//   }
-//   else{
-//   	$('.contact').fadeOut(500,'linear');
-//   	$('.contact').hide(0);
-//   	$('.about').show();
+$("*").on('touchstart', function(e){
+  console.log(e);
+  console.log(count);
+  count = count + (e.originalEvent.deltaY)/3;
+  $('.second').toggleClass('second first');
+  $('.hidden').removeClass('hidden');
+  if(count >= 150 ){
+  	$('.about').fadeOut(500,'linear');
+  	$('.about').hide(0);
+  	$('.contact').show();
+  }
+  else{
+  	$('.contact').fadeOut(500,'linear');
+  	$('.contact').hide(0);
+  	$('.about').show();
   	
-//   }
-//   if(count > 170){
-//   	count = 165;
-//   }
-//   if(count <= 0){
-//   	count = 50;
-//   }
+  }
+  if(count > 170){
+  	count = 165;
+  }
+  if(count <= 0){
+  	count = 50;
+  }
  
-// });
+});
 
 // $(".test").trigger(event);
 
@@ -38,33 +39,3 @@ $('.contactLink').click(function(){
   	$('.about').hide(0);
   	$('.contact').show();
 });
- //touch screen
-
-
-if("ontouchstart" in window){
-   el.addEventListener('touchstart', touchMoveHandler);
-}
-
-touchMoveHandler = function(e){
-  console.log(count);
-  count = count + (e.originalEvent.deltaY)/3;
-  $('.second').toggleClass('second first');
-  $('.hidden').removeClass('hidden');
-  if(count >= 150 ){
-    $('.about').fadeOut(500,'linear');
-    $('.about').hide(0);
-    $('.contact').show();
-  }
-  else{
-    $('.contact').fadeOut(500,'linear');
-    $('.contact').hide(0);
-    $('.about').show();
-    
-  }
-  if(count > 170){
-    count = 165;
-  }
-  if(count <= 0){
-    count = 50;
-  }
-}
